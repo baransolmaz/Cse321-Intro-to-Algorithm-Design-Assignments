050fefa85af6335d02f110f94a04f6b5ff4554bb
Baran Solmaz <80854458+baransolmaz@users.noreply.github.com>
2022-02-02 21:24:08 +0300

    Delete CSE321_HW3_1801042601 directory

--- CSE321_HW3_1801042601/polynomial_1801042601.py
+++
@@ -1,8 +0,0 @@
-def func(arr,x):
-    result=0
-    for i in range(0,len(arr)):
-        result += (arr[len(arr)-1-i]*(x**i))
-    return result
-
-arr=[1,2,3,4]#kat sayılar x^3 + 2x^2 + 3x + 4    
-print(func(arr,2))    
